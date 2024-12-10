f9abf8164071b7059195fb132f249d86e153e86a
abdulgamal <gamala@paysokosystems.com>
2024-12-10 10:32:57 +0300

    setup with tailwind

---
+++ App.jsx
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import './global.css';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Login from './screens/login';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle} className="flex-1">
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Login />
+    </SafeAreaView>
+  );
+}
+
+export default App;
